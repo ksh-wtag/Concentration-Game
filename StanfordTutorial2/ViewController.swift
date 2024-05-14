6ef170347d23087ac4391c08adbbd4e780bdb478
Ksh-wtag <shakawat.ratul@welldev.io>
2024-05-14 11:48:10 +0600

    Initial Commit

---
+++ StanfordTutorial2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StanfordTutorial2
+//
+//  Created by Kazi Shakawat Hossain Ratul on 14/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
